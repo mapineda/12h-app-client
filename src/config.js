7dc5b26182fd82356da0e9675f21f0cb0006304a
mapineda <mapineda1222@gmail.com>
2017-11-06 22:23:21 -0600

    hooking up api

--- src/config.js
+++ src/config.js
@@ -2,6 +2,12 @@ export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   cognito: {
     USER_POOL_ID: "us-east-1_RtTRgpg6Y",
-    APP_CLIENT_ID: "397cr0h4h7imm23rl5ue2ht0bv"
+    APP_CLIENT_ID: "397cr0h4h7imm23rl5ue2ht0bv",
+    REGION: "us-east-1",
+    IDENTITY_POOL_ID: "us-east-1_RtTRgpg6Y",
+  },
+  apiGateway: {
+    URL: "https://tfzhi05w63.execute-api.us-east-1.amazonaws.com/prod-serverless-rest-12h",
+    REGION: "us-east-1"
   }
 };
